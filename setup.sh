41898286368d66e62b07a074b815524494488f4e
Sumit Tiwari <134449481+sumittiwari5@users.noreply.github.com>
2026-05-24 01:53:43 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,35 @@
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s \
+https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Java
+sudo apt install openjdk-21-jdk -y
+
+# Maven
+sudo apt install maven -y
+
+# Jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y
+sudo apt install jenkins -y
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Add users to docker group
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
